2bccb96c6de2d94d8151f6c9d8c190691f489d0d
raz <raz@infinityar.com>
2018-02-05 12:00:14 +0000

    deleted:    arch/arm64/kvm/hyplet/test/user_hyplet.h 	new file:   include/linux/hyplet_user.h

--- arch/arm64/kvm/hyplet/test/user_hyplet.h
+++
@@ -1,9 +0,0 @@
-#ifndef __USER_HYPLET_H_
-#define __USER_HYPLET_H_
-
-/*
- * hyplet_user.h is a sanitized hearer
-*/
-#include <linux/hyplet_user.h>
-
-#endif
---
+++ include/linux/hyplet_user.h
@@ -0,0 +1,32 @@
+#ifndef __HYPLET_USER_H__
+#define __HYPLET_USER_H__
+
+typedef enum { HYPLET_MAP_CODE = 1,
+	   HYPLET_MAP_STACK = 2,
+	   HYPLET_MAP_ANY= 3,
+	   HYPLET_TRAP_IRQ = 4,
+	   HYPLET_UNTRAP_IRQ = 5,
+	   HYPLET_REGISTER_BH = 6, // register the task to wake up
+}hyplet_ops;
+
+
+struct hyplet_map_addr {
+	unsigned long addr  __attribute__ ((packed));
+	int size   __attribute__ ((packed));
+};
+
+struct hyplet_irq_action {
+	int action;
+	int irq __attribute__ ((packed));
+};
+
+struct hyplet_ctrl {
+	int cmd  __attribute__ ((packed));
+	union  {
+		struct hyplet_map_addr addr ;
+		int irq;
+	}__action  __attribute__ ((packed));
+};
+
+#endif
+
