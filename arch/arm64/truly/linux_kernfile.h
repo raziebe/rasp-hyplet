310e0483f55462afd6075a290a87db31ac0a87b2
raz <raziebe@gmail.com>
2018-08-19 21:52:29 +0300

    deleted

--- arch/arm64/truly/linux_kernfile.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct file;
-
-struct file* file_open (const char* path, int flags, int rights);
-void file_close(struct file* file);
-size_t file_read(struct file* file, unsigned long long offset, unsigned char* data, size_t size);
-size_t file_write(struct file* file, unsigned char* data, size_t size);
-size_t file_write_foffset(struct file* file, unsigned char* data, size_t size);
