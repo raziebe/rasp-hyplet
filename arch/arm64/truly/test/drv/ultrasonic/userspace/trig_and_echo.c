84c551312046fa50315bc001a41f659b5d890e3e
raz <raziebe@gmail.com>
2018-09-18 22:17:21 +0300

    modified:   trig_and_echo.c

--- arch/arm64/truly/test/drv/ultrasonic/userspace/trig_and_echo.c
+++ arch/arm64/truly/test/drv/ultrasonic/userspace/trig_and_echo.c
@@ -8,19 +8,26 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
- #include <fcntl.h>
+#include <fcntl.h>
 
 const char *gpio475="/sys/class/gpio/gpio475/value"; // the trigger
 const char *gpio485="/sys/class/gpio/gpio485/value"; // the echo
 
-#include "utils.h"
+
+static inline long cycles_us(void)
+{
+	struct timeval t;
+
+	gettimeofday(&t);
+	return t.tv_sec * 1000000 + t.tv_usec;
+}
 
 int trig(char *onoff)
 {
 	int b;
 	int fd;
 
-	fd = open(gpio485, O_WRONLY);
+	fd = open(gpio475, O_WRONLY);
 	if (fd < 0){
 		perror("Failed to open gpio485 file");
 		return -1;
@@ -38,12 +45,12 @@ int trig(char *onoff)
 
 long wait_echo(char c)
 {
-	long t;
+	long t = 0;
         int b;
         int fd;
         char buf[32];
 wait:
-        fd = open(gpio475, O_RDONLY);
+        fd = open(gpio485, O_RDONLY);
         if (fd < 0){
                 perror("Failed to open gpio475 file");
                 return ;
@@ -55,10 +62,9 @@ wait:
                 close(fd);
                 goto wait;
         }
-
-	t = cycles_ns();
         close(fd);
-        if (buf[0] == c) {
+        if (buf[0] != c) {
+		t = cycles_us();
                 goto wait;
         }
 	return t;
@@ -68,23 +74,31 @@ int main(int argc,char *argv[])
 {
 	int i;
 	int sleep_us;
-	long s, e, dt_ms;
+	long s, e, dt_ms, tmp;
 	float supersonic_speed_ms = 343;// centimeter/milli;	
 	float distance;
 
 	if (argc < 2) {
+		printf("%s <wait time us>\n",argv[0]);
 		return -1;
 	}
+	
 	sleep_us = atoi(argv[1]);
 	trig("1\n");
 	// wait trigger
 	usleep(sleep_us);
 	trig("0\n");
-	s  = wait_echo('0');
-	e  = wait_echo('1');
+	s =  cycles_us();
+	tmp = wait_echo('0');
+	if (tmp != 0)
+		s = tmp;
+	tmp  = wait_echo('1');
+	if (tmp != 0)
+		e = tmp;
 	dt_ms = (e - s)/1000000;
+
+	distance  = ((float)dt_ms * supersonic_speed_ms)/2;
 	
-	distance  = (float)dt_ms * 17150;
-	
-	printf("distance %f cm\n",distance);
+	printf("distance %f cm dt=%ld (e-s)=%ld\n",
+		distance, dt_ms, e-s);
 }
