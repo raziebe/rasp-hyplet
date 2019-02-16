966df88de51398d4dce0e9e5352b974164d2784e
raz <raziebe@gmail.com>
2019-02-16 22:05:44 +0200

    add offset 0x10

--- arch/arm64/hyplet/virt_dw3_usb.c
+++ arch/arm64/hyplet/virt_dw3_usb.c
@@ -22,26 +22,57 @@ void prepare_special_addresses(struct hyplet_vm *vm)
 	 * Now prepare the real actions
 	 */
 	hyphnd = &vm->dev_access->hyphnd[0];
+	hyphnd->offset = 0xb00;
+	hyphnd->action = (__action__mmio__)KERN_TO_HYP(dwc3_mmio_abrt_actionb00);
+	/* GRSTCTL */
+	hyphnd = &vm->dev_access->hyphnd[1];
 	hyphnd->offset = 0x10;
-	hyphnd->action = dwc3_mmio_abrt_action10;
+	hyphnd->action = (__action__mmio__)KERN_TO_HYP(dwc3_mmio_abrt_action10);
 }
 
 
 void malware_prep_mmio(char *addr)
 {
-	memset(addr, 0xeeeeffff, PAGE_SIZE);
+	/* A marker */
+	memset(addr, 0xeeeeeeee, PAGE_SIZE);
 	mb();
 }
 
-void __hyp_text dwc3_mmio_abrt_action10(struct hyplet_vm *vm, struct hyplet_driver_handler *hyphnd)
+void __hyp_text set_val(char *p,char val,int len)
 {
-	unsigned long *p;
-	unsigned long el2_mmio_addr = el2_fault_address() | HYP_PAGE_OFFSET_LOW_MASK;
+	int i;
+	for (i = 0; i < len; i++)
+		p[i] = val;
+}
 
-	p = (unsigned long *)(el2_mmio_addr + hyphnd->offset);
-	if ( (*p & 0x1) )
-		(	*p) &= ~(0x1);
-		else
-			(*p) |= 0x1;
+/*
+ * An example for how the user can access an modify
+ * MMIO mapped page.
+ */
+void __hyp_text dwc3_mmio_abrt_actionb00(struct hyplet_vm *vm, struct hyplet_driver_handler *hyphnd)
+{
+	struct virt_dev_access* virtdev = 
+		(struct virt_dev_access*)KERN_TO_HYP(vm->dev_access);
+	unsigned char * el2_mmio_addr = (char *) KERN_TO_HYP(virtdev->faddr.fake_vaddr);
+	unsigned char *p;
+/* do nothing we rely on setting usb.firmware */
+	return;
+}
 
+/* GRSTCTL */
+void __hyp_text dwc3_mmio_abrt_action10(struct hyplet_vm *vm, 
+	struct hyplet_driver_handler *hyphnd)
+{
+	struct virt_dev_access* virtdev = 
+		(struct virt_dev_access*)KERN_TO_HYP(vm->dev_access);
+	unsigned char * el2_mmio_addr = (char *) KERN_TO_HYP(virtdev->faddr.fake_vaddr);
+	unsigned char *p;
+
+	p = (el2_mmio_addr + hyphnd->offset);
+
+	if (p[0] == 0) {
+		set_val(p,0xFF,8);
+	} else {
+		set_val(p,0x00,8);
+	}
 }
